bdb823414df9c4649bb856d9a966d11ec59ab27e
Milena Hamerski <hamerskimilena@gmail.com>
2024-12-01 17:59:51 -0300

    Rotas do componente contador e destinos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './view/home/home.component'; 
+import { DestinosEmAltaComponent } from './view/destinos-em-alta/destinos-em-alta.component'; 
+import { ContadorViagemComponent } from './view/contador-viagem/contador-viagem.component'; 
+
+
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
+  { path: 'destinos', component: DestinosEmAltaComponent }, 
+  { path: 'contador', component: ContadorViagemComponent }, 
+
+
+
 ];
 
 @NgModule({
